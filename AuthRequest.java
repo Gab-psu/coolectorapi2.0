0cf7d5bd564e783e5c3112105cd38eb87673c653
Gab-psu <gpb5236@psu.edu>
2024-11-03 09:46:27 -0500

    Update AuthRequest.java

--- AuthRequest.java
+++ AuthRequest.java
@@ -1,52 +1,23 @@
-// AuthRequest.java
-package com.example.payment.dto;
+package com.mycompany.paymentfacilitatorproject.dto;
 
 public class AuthRequest {
     private String username;
     private String password;
 
     // Getters and Setters
-}
-
-// AuthResponse.java
-package com.example.payment.dto;
-
-public class AuthResponse {
-    private String token;
-
-    public AuthResponse(String token) {
-        this.token = token;
+    public String getUsername() {
+        return username;
     }
 
-    // Getters and Setters
-}
-
-// PaymentRequest.java
-package com.example.payment.dto;
-
-import java.math.BigDecimal;
-
-public class PaymentRequest {
-    private BigDecimal amount;
-
-    // Getters and Setters
-}
-
-// PaymentResponse.java
-package com.example.payment.dto;
-
-public class PaymentResponse {
-    private String transactionId;
-    private String message;
-
-    public PaymentResponse(String transactionId) {
-        this.transactionId = transactionId;
-        this.message = "Transaction successful";
+    public void setUsername(String username) {
+        this.username = username;
     }
 
-    public PaymentResponse(String message) {
-        this.message = message;
+    public String getPassword() {
+        return password;
     }
 
-    // Getters and Setters
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
